36685462ba5e174d49578b872b743cb66b968bb4
Robin Giannattasio <robin.giannattasio@gmail.com>
2023-12-10 14:42:36 -0500

    Use stringified object input

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 const core = require('@actions/core');
 
-// const exampleResponse = {
-//     tokenUsages: 10,
-//     componentUsage: 12,
+const exampleResponse = {
+    tokenUsages: 10,
+    componentUsage: 12,
 
-//     // derivative data
-//     componentUsagesFromJsx: 10,
-//     componentUsagesFromHbs: 2,
-//     tokenUsagesFromScss: 6,
-//     tokenUsagesFromJsx: 4
-// }
+    // derivative data
+    componentUsagesFromJsx: 10,
+    componentUsagesFromHbs: 2,
+    tokenUsagesFromScss: 6,
+    tokenUsagesFromJsx: 4
+}
 
 const { getInput, summary } = core;
 
 // https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
 // https://github.com/actions/toolkit/pull/1574/files
-const generateMarkup = async (tokenUsage, componentUsage) => {
+const generateMarkup = async (numTokens, numComponents) => {
     await summary
      .addHeading('✨ EDS Usage ✨')
      .addRaw('Some content here :speech_balloon:', true)
      .addTable(
         [
             [{data: 'Token', header: true}, {data: 'Component', header: true}],
-            [`${tokenUsage}`, `${componentUsage}`]
+            [`${numTokens}`, `${numComponents}`]
         ]
      )
      .write();
 }
 
 try {
-  const tokens = getInput('tokens');
-  const components = getInput('components');
-  console.log('tokens', tokens);
-  console.log('components', components);
-
   const currentCount = JSON.parse(getInput('currentCount') || `{}`);
+  const { tokenUsages = 0, componentUsage = 0 } = currentCount;
   console.log('currentCount', currentCount);
 
-  generateMarkup(tokens, components);
+  generateMarkup(tokenUsages, componentUsage);
 } catch (error) {
   setFailed(error.message);
 }
