d5e554ac4c2aec8f9e5ea50cae8d07cd81e7446b
Robin Giannattasio <robin.giannattasio@gmail.com>
2023-12-10 16:59:25 -0500

    Make it prettier

--- index.js
+++ index.js
@@ -13,9 +13,19 @@ const core = require('@actions/core');
 
 const { getInput, summary } = core;
 
-// https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
-// https://github.com/actions/toolkit/pull/1574/files
-const generateMarkup = async (data) => {
+const getStatusMessage = (current, prev) => {
+  // Assume increase means good, status quo means fine, and decrease is bad
+  const result = current - prev;
+  if (result > 0) return '🟢';
+  if (result < 0) return '🟠';
+  return '⚪️'
+};
+
+const getChangeMessage = (current, prev) => {
+  return `${current - prev}${current > prev ? '🔥' : ''}`
+}
+
+const generateMarkup = async (current, prev) => {
   const {
     tokenUsages = 0,
     componentUsages = 0,
@@ -23,21 +33,47 @@ const generateMarkup = async (data) => {
     componentUsagesFromHbs = 0,
     tokenUsagesFromScss = 0,
     tokenUsagesFromJsx = 0,
-  } = data;
+  } = current;
 
+  const {
+    tokenUsages: prevTokenUsages = 0,
+    componentUsages: prevComponentUsages = 0,
+    // componentUsagesFromJsx: prevComponentUsagesFromJsx = 0,
+    // componentUsagesFromHbs: prevComponentUsagesFromHbs = 0,
+    // tokenUsagesFromScss: prevTokenUsagesFromScss = 0,
+    // tokenUsagesFromJsx: prevTokenUsagesFromJsx = 0,
+  } = prev;
+
+
+  // https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
+  // https://github.com/actions/toolkit/pull/1574/files
   await summary
     .addHeading('✨ EDS Usage ✨')
-    .addRaw('Helpful information about this chart and why would want to use it: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', true)
-    .addBreak()
-    .addTable(
+    .addRaw('Successful adoption of the Expel Design System will allow us to recalibrate our UX and unify our offering across product lines. The following chart tracks usage of the different EDS assets throughout this repository.')
+    .addEOL()
+    .addTable([
       [
-          [{data: 'Token', header: true}, {data: 'Component', header: true}],
-          [`${tokenUsages}`, `${componentUsages}`]
-      ]
-    )
+        {data: 'Asset ⭐️', header: true},
+        {data: 'Usage #️⃣', header: true},
+        {data: 'Change 📈', header: true},
+        {data: 'Status 🌈', header: true},
+      ],[
+        `Tokens`,
+        `${tokenUsages}`,
+        getChangeMessage(tokenUsages, prevTokenUsages),
+        getStatusMessage(tokenUsages, prevTokenUsages),
+      ],[
+        `Components`,
+        `${componentUsages}`,
+        getChangeMessage(tokenUsages, prevTokenUsages),
+        getStatusMessage(componentUsages, prevComponentUsages)
+    ]])
+    .addRaw('For more information about usage of assets over time, please visit ')
+    .addLink('the EDS usage chart', 'https://docs.google.com/spreadsheets/d/1G4URwMwPY2uWxeV4PNKLXBUoSr75EroaqVyFZRJ35FY/edit#gid=0https://docs.google.com/spreadsheets/d/1G4URwMwPY2uWxeV4PNKLXBUoSr75EroaqVyFZRJ35FY/edit#gid=0')
+    .addEOL()
     .addHeading('Token Breakdown', '2')
     .addRaw('Tokens can be utilized in scoped globally or at the component level. The following table summarizes complete token usage.')
-    .addBreak()
+    .addEOL()
     .addTable(
       [
         [{data: 'Global', header: true}, {data: 'Component', header: true},  {data: 'Total', header: true}],
@@ -46,10 +82,10 @@ const generateMarkup = async (data) => {
     )
     .addHeading('Component Breakdown', '2')
     .addRaw('Design System Components can be found in Ember Templates or in React. This table breaks down the component usage across the code base.')
-    .addBreak()
+    .addEOL()
     .addTable(
       [
-        [{data: 'React', header: true}, {data: 'Ember', header: true},  {data: 'Total', header: true}],
+        [{data: 'React', header: true}, {data: 'Ember', header: true},  {data: 'Total #️⃣', header: true}],
         [`${componentUsagesFromJsx}`, `${componentUsagesFromHbs}`, `${componentUsages}`]
       ]
     )
@@ -57,11 +93,11 @@ const generateMarkup = async (data) => {
 }
 
 try {
-  console.log('currentCount - raw format', getInput('currentCount'));
-  const currentCount = JSON.parse(getInput('currentCount') || `{}`);
-  console.log('currentCount - post parse', currentCount);
+  console.log('currentCount', getInput('currentCount'));
+  const currentCount = JSON.parse(getInput('currentCount') || '{}');
+  const prevCount = JSON.parse(getInput('previousCount') || '{}');
 
-  generateMarkup(currentCount);
+  generateMarkup(currentCount, prevCount);
 } catch (error) {
   core.setFailed(error.message);
 }
