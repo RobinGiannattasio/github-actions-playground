82ed7a4485eeff69b48feafc74fe57b321e28115
Robin Giannattasio <robin.giannattasio@gmail.com>
2023-12-10 14:25:05 -0500

    Accept info from workflow inputs

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 const core = require('@actions/core');
 
-const mockResponse = {
-    tokenUsages: 10,
-    componentUsage: 12,
+// const exampleResponse = {
+//     tokenUsages: 10,
+//     componentUsage: 12,
 
-    // derivative data
-    componentUsagesFromJsx: 10,
-    componentUsagesFromHbs: 2,
-    tokenUsagesFromScss: 6,
-    tokenUsagesFromJsx: 4
-}
+//     // derivative data
+//     componentUsagesFromJsx: 10,
+//     componentUsagesFromHbs: 2,
+//     tokenUsagesFromScss: 6,
+//     tokenUsagesFromJsx: 4
+// }
+
+const { getInput, summary } = core;
 
 // https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
 // https://github.com/actions/toolkit/pull/1574/files
-const generateMarkup = async (response) => {
-    console.log('response data', response);
-    const { summary } = core;
+const generateMarkup = async (tokenUsage, componentUsage) => {
+    console.log('tokenUsage', tokenUsage);
+    console.log('componentUsage', componentUsage);
     await summary
-     .addHeading('EDS Usage')
+     .addHeading('✨ EDS Usage ✨')
+     .addRaw('Some content here :speech_balloon:', true)
      .addTable(
         [
             [{data: 'Token', header: true}, {data: 'Component', header: true}],
-            [`${response.tokenUsages}`, `${response.componentUsage}`]
+            [`${tokenUsage}`, `${componentUsage}`]
         ]
      )
      .write();
 }
 
 try {
-  generateMarkup(mockResponse);
+  const tokens = getInput('tokens');
+  const components = getInput('components');
+  console.log('tokens', tokens);
+  console.log('components', components);
+  generateMarkup(tokens, components);
 } catch (error) {
   setFailed(error.message);
 }
